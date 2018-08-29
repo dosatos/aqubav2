fc47cff28bb8169ef5362a9727d8aa09caa6c174
yb2n17@soton.ac.uk <yb2n17@soton.ac.uk>
2018-08-30 00:26:58 +0100

    opening a question from homepage test dadded

--- lessons/tests/functional_tests.py
+++ lessons/tests/functional_tests.py
@@ -6,8 +6,8 @@ import bs4
 class TestMain(unittest.TestCase):
 
     def setUp(self):  #
-        url = 'http://127.0.0.1:8000/'
-        r = requests.get(url)
+        self.url = 'http://127.0.0.1:8000/'
+        r = requests.get(self.url)
         self.html = bs4.BeautifulSoup(r.text)
 
 
@@ -16,6 +16,12 @@ class TestMain(unittest.TestCase):
         # He goes to check out its homepage
         # He notices the page title and header mention Aquba
         self.assertEqual("Aquba", self.html.title.text)
+        # # He sees several questions and opens the first one
+        quest_container = self.html.find("div", {"class": "quest-container"})
+        html = requests.get(self.url + quest_container.a['href'])
+        print(html.text)
+        self.assertEqual(1, html.text)
+
         self.fail('Finish the test!') #
         # She is invited to enter a to-do item straight away
 
